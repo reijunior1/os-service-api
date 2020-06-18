ab7cbdd89e01bf99304cee2034137870b9043a3a
reijunior1 <reijunior1@hotmail.com>
2020-06-18 10:34:35 -0300

    iniciando-projeto

---
+++ src/main/java/api/ordemservico/OrdemServicoApiApplication.java
@@ -0,0 +1,13 @@
+package api.ordemservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemServicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemServicoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/ordemservico/OrdemServicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.ordemservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemServicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
