4ca0026cddc06434af1007f2304edd0b718c760d
reijunior1 <reijunior1@hotmail.com>
2020-06-18 17:16:46 -0300

    integrando MYSQL

--- src/main/java/api/ordemservico/api/controller/ClienteController.java
+++ src/main/java/api/ordemservico/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package api.ordemservico.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,22 +14,13 @@ import api.ordemservico.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Reinaldo Couto");
-		cliente1.setTelefone("199999999");
-		cliente1.setEmail("reijunior1@hotmail.com");
-		
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Reinaldo");
-		cliente2.setTelefone("19-99998888");
-		cliente2.setEmail("reijunior1@hotmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
--- src/main/java/api/ordemservico/domain/model/Cliente.java
+++ src/main/java/api/ordemservico/domain/model/Cliente.java
@@ -1,10 +1,22 @@
 package api.ordemservico.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
+	
+	@Column(name = "telefone")
 	private String telefone;
 	
 	public Long getId() {
@@ -31,7 +43,28 @@ public class Cliente {
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
